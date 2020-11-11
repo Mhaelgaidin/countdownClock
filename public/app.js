d9a8f0ed796b16b7f201ff66b57be1ee9d53fadd
Mhaelgaidin <d_hooper@hotmail.co.uk>
2020-11-11 16:05:20 +0000

    create events

--- public/app.js
+++ public/app.js
@@ -1,1 +1,29 @@
-console.log('ready')
+const events = []
+
+document.querySelector('#newEvent').addEventListener('submit', (e) => {
+  e.preventDefault()
+  let newEvent = {
+    name: e.target.eventName.value,
+    date: new Date(e.target.eventDate.value + ' ' + e.target.eventTime.value),
+  }
+
+  if (validDate(newEvent.date)) {
+    document.querySelector('#warning').classList.add('hidden')
+    events.push(newEvent)
+    console.log(events)
+  } else {
+    document.querySelector('#warning').classList.remove('hidden')
+  }
+})
+
+function validDate(newDate) {
+  let currentDate = new Date()
+  if (newDate < currentDate) {
+    return false
+  }
+  if (newDate <= currentDate && newDate.getTime() < currentDate.getTime()) {
+    return false
+  } else {
+    return true
+  }
+}
